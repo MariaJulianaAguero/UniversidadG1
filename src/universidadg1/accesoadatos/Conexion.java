fe7731423fa7ec2bdf31057b11f9e4aaaa24e498
ALAN PEÑIÑOREY <gapgonza@gmail.com>
2023-10-12 17:29:45 -0300

    .

--- src/universidadg1/accesoadatos/Conexion.java
+++ src/universidadg1/accesoadatos/Conexion.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadg1.accesoadatos;
 
 import java.sql.Connection;
@@ -13,36 +9,34 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 
-/**
- *
- * @author MI EQUIPO
- */
 public class Conexion {
-    private static final String URL= "jdbc:mariadb://localhost/";
-    private static final String DB= "universidadulp";
-    private static final String USUARIO= "root";
-    private static final String PASSWORD= "";
+
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidadulp";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
     private static Connection connection;
     private static DriverManager Connection;
 
     public Conexion() {
     }
-    
-    public static Connection getConexion(){
-        if(connection==null){
-            try{
+
+    public static Connection getConexion() {
+        if (connection == null) {
+            try {
                 Class.forName("org.mariadb.jdbc.Driver");
-                Connection =(DriverManager) DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
-                
+                Connection = (DriverManager) DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+
             } catch (ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "Error al cargar los Drivers");
-            }catch(SQLException sql){
+            } catch (SQLException sql) {
                 JOptionPane.showMessageDialog(null, "Error al conectarse a la BD");
                 if (sql.getErrorCode() == 1062) {
-                JOptionPane.showMessageDialog(null, "Ya exite un alumno con ese DNI");
+                    JOptionPane.showMessageDialog(null, "Ya exite un alumno con ese DNI");
+                }
             }
+            
         }
         return connection;
     }
-    
 }
