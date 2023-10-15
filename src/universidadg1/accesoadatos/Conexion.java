77646f23a35ad6cc8646c260dc8ce3a5353c57f3
sebastian castro <95104254+ssebasss@users.noreply.github.com>
2023-10-15 00:54:59 -0300

    Update Conexion.java
    conexion

--- src/universidadg1/accesoadatos/Conexion.java
+++ src/universidadg1/accesoadatos/Conexion.java
@@ -9,32 +9,32 @@ import javax.swing.JOptionPane;
 
 public class Conexion {
 
-    private static final String URL = "jdbc:mariadb://localhost/";
-    private static final String DB = "universidadulp";
+    private static final String URL = "jdbc:mariadb://localhost:3308/tp13_ulp";
     private static final String USUARIO = "root";
-    private static final String PASSWORD = "";
-    private static Connection connection;
-    private static DriverManager Connection;
+    private static  String PASSWORD = "";
+    private static Connection conexion;
+
 
     private Conexion() {
     }
-
-    public static Connection getConexion() {
-        if (connection == null) {
-            try {
-                Class.forName("org.mariadb.jdbc.Driver");
-                connection = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
-
-            } catch (ClassNotFoundException ex) {
-                JOptionPane.showMessageDialog(null, "Error al cargar los Drivers");
-            } catch (SQLException sql) {
-                JOptionPane.showMessageDialog(null, "Error al conectarse a la BD");
-                if (sql.getErrorCode() == 1062) {
+    
+    public static Connection getConexion(){
+        if(conexion==null){
+             try{                           
+            // cargar driver
+            Class.forName("org.mariadb.jdbc.Driver");
+           //conectar BD
+          conexion = DriverManager.getConnection(URL , USUARIO, PASSWORD); 
+           System.out.println("conectada BD");
+        }catch(ClassNotFoundException cnf){
+            JOptionPane.showMessageDialog(null,"Error al cargar driver");                     
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null,"Error al conectar BD");
+           if (ex.getErrorCode() == 1062) {
                     JOptionPane.showMessageDialog(null, "Ya exite un alumno con ese DNI");
                 }
-            }
-
+        }      
         }
-        return connection;
+        return conexion;      
     }
 }
